08c5d78a36c562fb18c8641f42f8cb19eb410272
Zenan Hu <zenan2048@gmail.com>
2026-01-31 01:46:29 +0800

    init react app

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,14 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      {"hello"}
+      <NxWelcome title="client" />
+    </div>
+  );
+}
+
+export default App;
